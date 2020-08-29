8fd8fa7258ef666c3ff0404483c3cee0eeeba52b
vikas <vikas@tradala.in>
2020-08-29 21:32:47 +0530

    completed image viewer

--- app/screens/ImageViewer.js
+++ app/screens/ImageViewer.js
@@ -1,26 +1,40 @@
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
+import colors from "../config/colors";
 export default function ImageViewer(props) {
   return (
     <View style={styles.container}>
       <View style={styles.closeIcon}></View>
-      <Image style={styles.image} source={require("../assets/chair.jpg")} />
+      <View style={styles.deleteIcon}></View>
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../assets/chair.jpg")}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  closeIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: "#fc5c65",
-    position: "absolute",
-    top: "40",
-    left: "30",
-  },
   container: {
     backgroundColor: "#000",
     flex: 1,
+  },
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.primary,
+    position: "absolute",
+    top: 40,
+    left: 30,
+  },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.secondary,
+    position: "absolute",
+    top: 40,
+    right: 30,
   },
   image: {
     width: "100%",
