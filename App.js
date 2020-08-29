8f5ef023c0110e8e3cc7b6823a49773f03cc8247
vikas <vikas@tradala.in>
2020-08-29 21:06:02 +0530

    added landing page & some part of image viewer

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Landing from "./app/screens/Landing";
+import ImageViewer from "./app/screens/ImageViewer";
+export default function App() {
+  // return <Landing />;
+  return <ImageViewer />
+}
